72296f3e6c46d3a17dbbc8b0ddd6ec73c48bcf8f
FlapJac-k <eslamce122@gmail.com>
2025-05-18 23:10:06 +0300

    Finished adding bids/notification to the client

--- app/auctions/details/[id]/BidItem.tsx
+++ app/auctions/details/[id]/BidItem.tsx
@@ -14,18 +14,21 @@ export default function BidItem({ bid }: Props) {
     switch (bid.bidStatus) {
       case "Accepted":
         bgColor = "bg-green-200";
-        text = "Bid Accepted";
+        text = "Bid accepted";
         break;
       case "AcceptedBelowReserve":
         bgColor = "bg-amber-500";
+        text = "Reserve not met";
+        break;
+      case "TooLow":
+        bgColor = "bg-red-200";
         text = "Bid was too low";
         break;
       default:
-        bgColor = "bg-red-500";
+        bgColor = "bg-red-200";
         text = "Bid placed after auction finished";
         break;
     }
-
     return { bgColor, text };
   }
 
@@ -39,12 +42,16 @@ export default function BidItem({ bid }: Props) {
     >
       <div className="flex flex-col">
         <span>Bidder: {bid.bidder}</span>
-        <span className="text-gray-700 text-sm">Time: {format(bid.bidTime, 'dd MMM yyyy h:mm a')}</span>
+        <span className="text-gray-700 text-sm">
+          Time: {format(bid.bidTime, "dd MMM yyyy h:mm a")}
+        </span>
       </div>
       <div className="flex flex-col text-right">
-        <div className="text-xl font-semibold">${numberWithCommas(bid.amount)}</div>
+        <div className="text-xl font-semibold">
+          ${numberWithCommas(bid.amount)}
+        </div>
         <div className="flex flex-row items-center">
-            <span>{getBidInfo().text}</span>
+          <span>{getBidInfo().text}</span>
         </div>
       </div>
     </div>
